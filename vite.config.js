c7949a200ae3b409a1b3eb8554172fa2bd6826b6
zixsorus <touy_elec@hotmail.com>
2026-04-06 11:17:10 +0700

    chore: update vite configuration settings

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,29 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
+  build: {
+    // เพิ่มลิมิตการเตือนขนาดไฟล์จาก 500kb เป็น 1500kb (1.5MB)
+    chunkSizeWarningLimit: 1500,
+    
+    // ตั้งค่า Code Splitting หั่นไฟล์ Library ให้โหลดแยกกัน
+    rollupOptions: { 
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            // แยกกลุ่มไอคอน
+            if (id.includes('lucide-react')) {
+              return 'vendor-icons';
+            }
+            // แยกกลุ่มไลบรารีอ่านไฟล์ Excel/CSV ที่มักจะไฟล์ใหญ่
+            if (id.includes('xlsx') || id.includes('papaparse')) {
+              return 'vendor-parser';
+            }
+            // ไลบรารีพื้นฐานอื่นๆ (React, ReactDOM)
+            return 'vendor-core';
+          }
+        }
+      }
+    }
+  }
 })
